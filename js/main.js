9c7391434f61a169767eed068d4a44a4f709bb23
Irokanade <michaeljs.leong@gmail.com>
2024-10-01 17:54:36 +0800

    feat: added chessboared where you can move pieces around and a random vs random match

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var board1 = ChessBoard('board1', {
+    draggable: true,
+    dropOffBoard: 'trash',
+    sparePieces: true
+});
+
+// NOTE: this example uses the chess.js library:
+// https://github.com/jhlywa/chess.js
+
+var board = null
+var game = new Chess()
+
+function makeRandomMove() {
+    var possibleMoves = game.moves();
+
+    // exit if the game is over
+    if (game.game_over()) {
+        return;
+    }
+
+    var randomIdx = Math.floor(Math.random() * possibleMoves.length);
+    game.move(possibleMoves[randomIdx]);
+    board.position(game.fen());
+
+    window.setTimeout(makeRandomMove, 500);
+}
+
+board = Chessboard('myBoard', 'start');
+
+window.setTimeout(makeRandomMove, 500);
